eeb064f755ff6056d1461321de1ac0c3f618679b
unknown <osama.albader99@gmail.com>
2024-11-05 15:06:51 +0300

    Inheritance

--- src/company/Main.java
+++ src/company/Main.java
@@ -5,9 +5,17 @@ public class Main {
         Employee emily = new Employee("Emily", "Finance", 50000);
         Manager john = new Manager("John", "IT", 75000, 5);
         Intern dave = new Intern("Dave", "Marketing", 30000, "State University");
+        Engineer engineer = new Engineer("osama", "digital forensics", 5000000000.0, new String[]{
+                "fast typer","slow learner","smart"
+        });
+        Director director = new Director("salim", "malware analysis",45000.0, 3000000.5);
+        SalesPerson person = new SalesPerson("fjri", "CLE",300.0, 230 );
 
         System.out.println(emily);
         System.out.println(john);
         System.out.println(dave);
+        System.out.println(engineer);
+        System.out.println(director);
+        System.out.println(person);
     }
 }
